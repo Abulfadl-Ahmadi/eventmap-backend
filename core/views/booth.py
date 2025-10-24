36f3945797be4d2f15b7f2661242f16add3df2e5
Abulfadl Ahmadi <abulfadl.ahmadi@gmail.com>
2025-10-24 21:35:23 +0330

    Implement get_queryset and list methods in BoothEventViewSet for filtered event retrieval

--- core/views/booth.py
+++ core/views/booth.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.response import Response
 from ..models.booth import Booth, BoothEvent, Rating, ContestSetting, ContestWinner
 from ..serializers.booth import BoothSerializer, BoothEventSerializer, RatingSerializer, ContestSettingSerializer, ContestWinnerSerializer
 
@@ -9,6 +10,22 @@ class BoothViewSet(viewsets.ModelViewSet):
 class BoothEventViewSet(viewsets.ModelViewSet):
     queryset = BoothEvent.objects.all()
     serializer_class = BoothEventSerializer
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        booth_pk = self.kwargs.get('booth_pk')
+        if booth_pk:
+            queryset = queryset.filter(booth_id=booth_pk)
+        return queryset
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        return Response({
+            'events': serializer.data,
+            'upcomingEvents': None,
+            'pastEvents': None,
+        })
 
 class RatingViewSet(viewsets.ModelViewSet):
     queryset = Rating.objects.all()
